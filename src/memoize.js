291ba3291ec3b9671897a8124f7d438158f009c1
Egor <ze99@rambler.ru>
2020-03-04 20:19:20 +0300

    begin js task 2

--- src/memoize.js
+++ src/memoize.js
@@ -1,19 +1,19 @@
 export function memoize(fn) { 
-    if (typeof fn === 'undefined' || typeof fn !== 'function') {
+    if (typeof fn === 'undefined' || typeof fn !== 'function' || fn == '{ }') {
         return null;
     } 
-    let cache = new Map(); 
+    let cache = new Map();  
     return (...args) => { 
-      for(let key of cache.keys()) {
+      for (let key of cache.keys()) {
             console.log("Cache");
-            if (compare(key,args)) {  
+            if (compare(key,args)) {   
                 return cache.get(key);  
-            }  
+            }   
         }
         console.log("Calculating");
         let result = fn(...args); 
         cache.set(args,result); 
-        return result;  
+        return result;   
     }   
   }  
        
@@ -21,23 +21,3 @@ function compare(a1, a2) {
     return a1.length == a2.length && a1.every((v,i)=>v == a2[i])
 }  
     
-// var add = (n) => (n + 10); 
-// var mult = (n,m) => (n * m);
-// function sum() {
-//     let result = 0;
-//     for (var i = 0; i < arguments.length; i++) { 
-//         result += arguments[i];
-//     } 
-//     return result;  
-// } 
-// //const memoizedAdd = memoize(add);
-// //const memoizedMult = memoize(mult);
-// const memoizedSum = memoize(sum); 
-// // console.log(memoizedAdd(3));  // вычислено  
-// // console.log(memoizedAdd(3));  // взято из кэша
-// // console.log(memoizedAdd(4));  // вычислено 
-// // console.log(memoizedAdd(4));  // взято из кэша 
-// // console.log(memoizedMult(4,4));  // вычислено 
-// // console.log(memoizedMult(4,4));  // взято из кэша 
-// console.log(memoizedSum(1,2,3)); 
-// console.log(memoizedSum(1,2,3));   
